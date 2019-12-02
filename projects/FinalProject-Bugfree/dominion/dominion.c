8d09e4ddbc4bb51250aa95f8e6e8390758cdc994
AC3912 <43506992+AC3912@users.noreply.github.com>
2019-12-02 16:00:37 -0500

    bug fix - if statement in case remodel
    changed from > to <
    Also changed case mine if statement from <= to <

--- projects/FinalProject-Bugfree/dominion/dominion.c
+++ projects/FinalProject-Bugfree/dominion/dominion.c
@@ -818,7 +818,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
             return -1;
         }
 
-        if ( (getCost(state->hand[currentPlayer][choice1]) + 3) <= getCost(choice2) )
+        if ( (getCost(state->hand[currentPlayer][choice1]) + 3) < getCost(choice2) )
         {
             return -1;
         }
@@ -843,7 +843,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     case remodel:
         j = state->hand[currentPlayer][choice1];  //store card we will trash
 
-        if ( (getCost(state->hand[currentPlayer][choice1]) + 2) > getCost(choice2) )
+        if ( (getCost(state->hand[currentPlayer][choice1]) + 2) < getCost(choice2) )
         {
             return -1;
         }
